29e2709b84e0c64d8b72b81508fd4b44617f1906
GURU <gurupdas11@gmail.com>
2022-11-23 16:27:41 +0530

    Add driver floder

---
+++ src/main/java/Driver/NewGITDriver.java
@@ -0,0 +1,10 @@
+package Driver;
+
+public class NewGITDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("practice git commands");
+	}
+
+}
